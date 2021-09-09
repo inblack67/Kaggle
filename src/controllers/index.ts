5f828003352335eb6e5b05739db2929b849627ac
ryuzaki <ryuzaki>
2021-09-09 18:55:18 +0530

    query fixes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -11,16 +11,7 @@ export const GetCountriesController = async (
   res: Response<any, ILocals>,
   __: NextFunction,
 ) => {
-  let countries: Country[];
-  const { startYear, endYear, category } = req.query;
-
-  if (category) {
-    countries = await res.locals.prisma.country.findMany({
-      where: {
-        category: { equals: category as string },
-      },
-    });
-  }
+  const { startYear, endYear } = req.query;
 
   if (startYear || endYear) {
     if (startYear && endYear) {
@@ -55,6 +46,7 @@ export const GetCountriesController = async (
     return;
   }
 
+  const countries = await res.locals.prisma.country.findMany();
   res.status(200).json({ success: true, data: countries! });
 };
 
@@ -64,17 +56,60 @@ export const GetCountryController = async (
   __: NextFunction,
 ) => {
   const { country_or_area } = req.params;
+  const { startYear, endYear, category } = req.query;
 
-  const countries = await res.locals.prisma.country.findMany({
+  let countries: Country[];
+
+  if (category) {
+    countries = await res.locals.prisma.country.findMany({
+      where: {
+        category: { contains: category as string },
+        country_or_area,
+      },
+    });
+  }
+
+  if (startYear || endYear) {
+    if (startYear && endYear) {
+      const filteredCountries = await res.locals.prisma.country.findMany({
+        where: {
+          country_or_area,
+          year: {
+            gte: +startYear,
+            lte: +endYear,
+          },
+        },
+      });
+      res.status(200).json({ success: true, data: filteredCountries });
+    } else if (startYear) {
+      const filteredCountries = await res.locals.prisma.country.findMany({
+        where: {
+          country_or_area,
+          year: {
+            gte: +startYear,
+          },
+        },
+      });
+      res.status(200).json({ success: true, data: filteredCountries });
+    } else if (endYear) {
+      const filteredCountries = await res.locals.prisma.country.findMany({
+        where: {
+          country_or_area,
+          year: {
+            lte: +endYear,
+          },
+        },
+      });
+      res.status(200).json({ success: true, data: filteredCountries });
+    }
+    return;
+  }
+
+  countries = await res.locals.prisma.country.findMany({
     where: {
       country_or_area,
     },
   });
-
-  if (countries.length === 0) {
-    res.status(404).json({ success: false, error: 'No such country exist' });
-    return;
-  }
 
   res.status(200).json({ success: true, data: countries });
 };
