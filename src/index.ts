6e266751e5f5ed9f43e20d33ce48cb2a7efa1a46
ryuzaki <ryuzaki>
2021-09-11 09:42:38 +0530

    docs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import { getMyRedisClient } from './cache';
 import { RootController } from './controllers/index';
 import { getMyPrismaClient } from './db';
@@ -7,6 +9,22 @@ import { populateLocals } from './middlewares/populateLocals';
 import countriesRoutes from './routes/countries';
 
 const main = async () => {
+  const specs = swaggerJSDoc({
+    definition: {
+      info: {
+        title: 'Countries API',
+        version: '3.0.0',
+        description: 'REST API for international greenhouse gas emissions',
+      },
+      servers: [
+        {
+          url: 'http://localhost:5000',
+        },
+      ],
+    },
+    apis: ['./routes/*.js'],
+  });
+
   const redis = getMyRedisClient();
   const prisma = getMyPrismaClient();
 
@@ -14,7 +32,10 @@ const main = async () => {
 
   app.use(morgan('dev'));
 
-  app.get('/', populateLocals({ redis, prisma }), RootController);
+  app.get('/ping', RootController);
+
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
+  
   app.use('/countries', populateLocals({ redis, prisma }), countriesRoutes);
 
   const PORT = process.env.PORT || 5000;
--- src/routes/countries.ts
+++ src/routes/countries.ts
@@ -3,7 +3,61 @@ import { GetCountriesController, GetCountryController } from '../controllers';
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Country:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         country_or_area:
+ *           type: string
+ *           description: The Country
+ *         year:
+ *           type: number
+ *           description: The year
+ *         value:
+ *           type: number
+ *           description: The value of greenhouse emissions
+ *         category:
+ *           type: string
+ *           description: The category
+ *       example:
+ *         id: 1
+ * 		   country_or_area: "Australia",
+ * 		   year: 2014,
+ * 		   value: 393126.946994288,
+ * 		   category: "carbon_dioxide_co2_emissions_without_land_use_land_use_change_and_forestry_lulucf_in_kilotonne_co2_equivalent",
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Countries
+ *   description: Get All Countries
+ */
+
+/**
+ * @swagger
+ * /countries:
+ *   get:
+ *     summary: Returns the list of all the countries
+ *     tags: [Country]
+ *     responses:
+ *       200:
+ *         description: The list of the country
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Country'
+ */
 router.get('/', GetCountriesController);
+
 router.get('/:id', GetCountryController);
 
 export default router;
